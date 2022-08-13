33d1d53832a92e95d1901c80c823fcbc8bdbfbc2
sirahm <62294690+sirahm@users.noreply.github.com>
2022-08-13 08:17:10 +0800

    first commit

---
+++ Server/Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb+srv://atiss:LbGnvmBQSqw8vaVf@cluster0.htmvx.mongodb.net/new_atiss?retryWrites=true&w=majority";
+exports.RemoteURI = process.env.RemoteURI;
+exports.HostName = "MongoDB Atlas";
+exports.Secret = process.env.SESSION_SECRET || "1af2ec08c869bdb42539bf93248ff528b742ff77";
+//# sourceMappingURL=db.js.map
---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb+srv://atiss:LbGnvmBQSqw8vaVf@cluster0.htmvx.mongodb.net/new_atiss?retryWrites=true&w=majority";
+export const RemoteURI = process.env.RemoteURI;
+export const HostName = "MongoDB Atlas";
+export const Secret = process.env.SESSION_SECRET || "1af2ec08c869bdb42539bf93248ff528b742ff77";
